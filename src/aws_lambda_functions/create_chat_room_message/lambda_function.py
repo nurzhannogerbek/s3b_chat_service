307a9fc438184c1c7a0f4e0e45078bdff0353995
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-11-26 13:03:32 +0600

    BT-321

--- src/aws_lambda_functions/create_chat_room_message/lambda_function.py
+++ src/aws_lambda_functions/create_chat_room_message/lambda_function.py
@@ -4,8 +4,6 @@ import logging
 import sys
 import os
 import uuid
-from cassandra.query import SimpleStatement, dict_factory
-from cassandra import ConsistencyLevel
 from psycopg2.extras import RealDictCursor
 
 """
@@ -273,14 +271,10 @@ def lambda_handler(event, context):
         'null' if quoted_message_content_url is None or len(quoted_message_content_url) == 0
         else "'{0}'".format(quoted_message_content_url)
     )
-    statement = SimpleStatement(
-        cassandra_query,
-        consistency_level=ConsistencyLevel.LOCAL_QUORUM
-    )
 
     # Execute a previously prepared CQL query.
     try:
-        cassandra_connection.execute(statement)
+        cassandra_connection.execute(cassandra_query)
     except Exception as error:
         logger.error(error)
         sys.exit(1)
@@ -313,20 +307,13 @@ def lambda_handler(event, context):
             channel_id,
             chat_room_id
         )
-        statement = SimpleStatement(
-            cassandra_query,
-            consistency_level=ConsistencyLevel.LOCAL_QUORUM
-        )
 
         # Execute a previously prepared CQL query.
         try:
-            cassandra_connection.execute(statement)
+            cassandra_connection.execute(cassandra_query)
         except Exception as error:
             logger.error(error)
             sys.exit(1)
-
-    # Return each row as a dictionary after querying the Cassandra database.
-    cassandra_connection.row_factory = dict_factory
 
     # Prepare the CQL query statement that returns the information of the created message.
     cassandra_query = '''
