51958fafad226d8a0984a7f297e032560b46576e
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-12-18 01:40:53 +0600

    BT-327

--- src/aws_lambda_functions/update_message_status/lambda_function.py
+++ src/aws_lambda_functions/update_message_status/lambda_function.py
@@ -1,39 +1,120 @@
+import logging
+import os
+from cassandra.cluster import Session
+from typing import *
+import uuid
+from threading import Thread
+from queue import Queue
 import databases
 import utils
-import logging
-import sys
-import os
 
+# Configure the logging tool in the AWS Lambda function.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
-"""
-Define connections to databases outside of the "lambda_handler" function.
-Connections to databases will be created the first time the function is called.
-Any subsequent function call will use the same database connections.
-"""
-cassandra_connection = None
-
-# Define databases settings parameters.
+# Initialize constants with parameters to configure.
 CASSANDRA_USERNAME = os.environ["CASSANDRA_USERNAME"]
 CASSANDRA_PASSWORD = os.environ["CASSANDRA_PASSWORD"]
-CASSANDRA_HOST = os.environ["CASSANDRA_HOST"].split(',')
+CASSANDRA_HOST = os.environ["CASSANDRA_HOST"].split(",")
 CASSANDRA_PORT = int(os.environ["CASSANDRA_PORT"])
 CASSANDRA_LOCAL_DC = os.environ["CASSANDRA_LOCAL_DC"]
 CASSANDRA_KEYSPACE_NAME = os.environ["CASSANDRA_KEYSPACE_NAME"]
 
-logger = logging.getLogger(__name__)  # Create the logger with the specified name.
-logger.setLevel(logging.WARNING)  # Set the logging level of the logger.
+
+# The connection to the database will be created the first time the AWS Lambda function is called.
+# Any subsequent call to the function will use the same database connection until the container stops.
+CASSANDRA_CONNECTION = None
 
 
-def lambda_handler(event, context):
-    """
-    :argument event: The AWS Lambda uses this parameter to pass in event data to the handler.
-    :argument context: The AWS Lambda uses this parameter to provide runtime information to your handler.
-    """
-    # Since connections with databases were defined outside of the function, we create global variables.
-    global cassandra_connection
-    if not cassandra_connection:
+def run_multithreading_tasks(functions: List[Dict[AnyStr, Union[Callable, Dict[AnyStr, Any]]]]) -> Dict[AnyStr, Any]:
+    # Create the empty list to save all parallel threads.
+    threads = []
+
+    # Create the queue to store all results of functions.
+    queue = Queue()
+
+    # Create the thread for each function.
+    for function in functions:
+        # Check whether the input arguments have keys in their dictionaries.
         try:
-            cassandra_connection = databases.create_cassandra_connection(
+            function_object = function["function_object"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+        try:
+            function_arguments = function["function_arguments"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+
+        # Add the instance of the queue to the list of function arguments.
+        function_arguments["queue"] = queue
+
+        # Create the thread.
+        thread = Thread(target=function_object, kwargs=function_arguments)
+        threads.append(thread)
+
+    # Start all parallel threads.
+    for thread in threads:
+        thread.start()
+
+    # Wait until all parallel threads are finished.
+    for thread in threads:
+        thread.join()
+
+    # Get the results of all threads.
+    results = {}
+    while not queue.empty():
+        results = {**results, **queue.get()}
+
+    # Return the results of all threads.
+    return results
+
+
+def check_input_arguments(**kwargs) -> None:
+    # Make sure that all the necessary arguments for the AWS Lambda function are present.
+    try:
+        input_arguments = kwargs["event"]["arguments"]["input"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        queue = kwargs["queue"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Check the format and values of required arguments in the list of input arguments.
+    required_arguments = ["chatRoomId", "messageId", "messageStatus"]
+    for argument_name, argument_value in input_arguments.items():
+        if argument_name not in required_arguments:
+            raise Exception("The '%s' argument doesn't exist.".format(utils.camel_case(argument_name)))
+        if argument_value is None:
+            raise Exception("The '%s' argument can't be None/Null/Undefined.".format(utils.camel_case(argument_name)))
+        if argument_name.endswith("Id"):
+            try:
+                uuid.UUID(argument_value)
+            except ValueError:
+                raise Exception("The '%s' argument format is not UUID.".format(utils.camel_case(argument_name)))
+
+    # Put the result of the function in the queue.
+    queue.put({
+        "input_arguments": {
+            "chat_room_id": input_arguments.get("chatRoomId", None),
+            "message_id": input_arguments.get("messageId", None),
+            "message_status": input_arguments.get("messageStatus", None)
+        }
+    })
+
+    # Return nothing.
+    return None
+
+
+def reuse_or_recreate_cassandra_connection(queue: Queue) -> None:
+    global CASSANDRA_CONNECTION
+    if not CASSANDRA_CONNECTION:
+        try:
+            CASSANDRA_CONNECTION = databases.create_cassandra_connection(
                 CASSANDRA_USERNAME,
                 CASSANDRA_PASSWORD,
                 CASSANDRA_HOST,
@@ -42,22 +123,20 @@ def lambda_handler(event, context):
             )
         except Exception as error:
             logger.error(error)
-            sys.exit(1)
+            raise Exception("Unable to connect to the Cassandra database.")
+    queue.put({"cassandra_connection": CASSANDRA_CONNECTION})
+    return None
 
-    # Define the values of the data passed to the function.
-    chat_room_id = event["arguments"]["input"]['chatRoomId']
-    message_id = event["arguments"]["input"]['messageId']
-    # Available values: "message_is_delivered", "message_is_read".
-    message_status = event["arguments"]["input"]['messageStatus']
 
-    # Set the name of the keyspace you will be working with.
-    # This statement must fix ERROR NoHostAvailable: ('Unable to complete the operation against any hosts').
-    success = False
-    while not success:
+def set_cassandra_keyspace(cassandra_connection: Session) -> None:
+    # This peace of code fix ERROR NoHostAvailable: ("Unable to complete the operation against any hosts").
+    successful_operation = False
+    while not successful_operation:
         try:
             cassandra_connection.set_keyspace(CASSANDRA_KEYSPACE_NAME)
-            success = True
-        except Exception as error:
+            successful_operation = True
+        except Exception as warning:
+            logger.warning(warning)
             try:
                 cassandra_connection = databases.create_cassandra_connection(
                     CASSANDRA_USERNAME,
@@ -68,85 +147,181 @@ def lambda_handler(event, context):
                 )
             except Exception as error:
                 logger.error(error)
-                sys.exit(1)
+                raise Exception(error)
 
-    # Prepare the CQL request that updates the status of the message.
-    cassandra_query = """
+    # Return nothing.
+    return None
+
+
+def update_chat_room_message_status(**kwargs) -> None:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        cassandra_connection = kwargs["cassandra_connection"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        cql_arguments = kwargs["cql_arguments"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Prepare the CQL query that updates the status of the message.
+    cql_statement = """
     update
         chat_rooms_messages
     set
-        {0} = true
+        %(column_name)s = true
     where
-        chat_room_id = {1}
+        chat_room_id = %(chat_room_id)s
     and
-        message_id = {2}
+        message_id = %(message_id)s
     if exists;
-    """.format(
-        message_status,
-        chat_room_id,
-        message_id
-    )
+    """
 
-    # Execute a previously prepared CQL query.
+    # Execute the CQL query dynamically, in a convenient and safe way.
     try:
-        cassandra_connection.execute(cassandra_query)
+        cassandra_connection.execute(cql_statement, cql_arguments)
     except Exception as error:
         logger.error(error)
-        sys.exit(1)
+        raise Exception(error)
 
-    # Prepare the CQL query statement that returns a certain number of recent messages from a particular chat room.
-    cassandra_query = '''
+    # Return nothing.
+    return None
+
+
+def get_chat_room_message_data(**kwargs) -> Dict[AnyStr, Any]:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        cassandra_connection = kwargs["cassandra_connection"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        cql_arguments = kwargs["cql_arguments"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Prepare the CQL query that gets data the last chat room message.
+    cql_statement = """
     select
         chat_room_id,
-        message_created_date_time,
-        message_updated_date_time,
-        message_deleted_date_time,
-        message_is_sent,
-        message_is_delivered,
-        message_is_read,
         message_id,
         message_author_id,
         message_channel_id,
-        message_type,
-        message_text,
         message_content_url,
-        quoted_message_id,
+        message_created_date_time,
+        message_deleted_date_time,
+        message_is_delivered,
+        message_is_read,
+        message_is_sent,
+        message_text,
+        message_type,
+        message_updated_date_time,
         quoted_message_author_id,
         quoted_message_channel_id,
-        quoted_message_type,
+        quoted_message_content_url,
+        quoted_message_id,
         quoted_message_text,
-        quoted_message_content_url
+        quoted_message_type
     from
         chat_rooms_messages
     where
-        chat_room_id = {0}
+        chat_room_id = %(chat_room_id)s
     and
-        message_id = {1}
+        message_id = %(message_id)s
     limit 1;
-    '''.format(
-        chat_room_id,
-        message_id
-    )
+    """
 
-    # Execute a previously prepared CQL query.
+    # Execute the CQL query dynamically, in a convenient and safe way.
     try:
-        chat_room_message_entry = cassandra_connection.execute(cassandra_query).one()
+        chat_room_message_data = cassandra_connection.execute(cql_statement, cql_arguments).one()
     except Exception as error:
         logger.error(error)
-        sys.exit(1)
+        raise Exception(error)
 
-    # Analyze the data about chat room message received from the database.
-    chat_room_message = dict()
-    if chat_room_message_entry is not None:
-        quoted_message = dict()
-        for key, value in chat_room_message_entry.items():
-            if ("_id" in key or "_date_time" in key) and value is not None:
-                value = str(value)
-            if "quoted_" in key:
-                quoted_message[utils.camel_case(key.replace("quoted_", ""))] = value
-            else:
-                chat_room_message[utils.camel_case(key)] = value
-        chat_room_message["quotedMessage"] = quoted_message
+    # Return data of the last chat room message.
+    return chat_room_message_data
 
-    # Return the the full information about chat room message as the response.
+
+def analyze_and_format_chat_room_message_data(**kwargs) -> Dict[AnyStr, Any]:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        chat_room_message_data = kwargs["chat_room_message_data"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Format the last message data.
+    chat_room_message = {}
+    quoted_message = {}
+    for key, value in chat_room_message_data.items():
+        if key.endswith("_date_time") and value is not None:
+            value = value.isoformat()
+        elif key.endswith("_id") and value is not None:
+            value = str(value)
+        if key.startswith("quoted_"):
+            quoted_message[utils.camel_case(key.replace("quoted_", ""))] = value
+        else:
+            chat_room_message[utils.camel_case(key)] = value
+    chat_room_message["quotedMessage"] = quoted_message
+
+    # Return analyzed and formatted chat room message data.
+    return chat_room_message
+
+
+def lambda_handler(event, context):
+    """
+    :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
+    :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
+    """
+    # Run several initialization functions in parallel.
+    results_of_tasks = run_multithreading_tasks([
+        {
+            "function_object": check_input_arguments,
+            "function_arguments": {
+                "event": event
+            }
+        },
+        {
+            "function_object": reuse_or_recreate_cassandra_connection,
+            "function_arguments": {}
+        }
+    ])
+
+    # Define the input arguments of the AWS Lambda function.
+    input_arguments = results_of_tasks["input_arguments"]
+    chat_room_id = uuid.UUID(input_arguments["chat_room_id"])
+    message_id = uuid.UUID(input_arguments["message_id"])
+    # Available values: "message_is_delivered", "message_is_read", "message_is_sent".
+    message_status = input_arguments['message_status']
+
+    # Define the instances of the database connections.
+    cassandra_connection = results_of_tasks["cassandra_connection"]
+    set_cassandra_keyspace(cassandra_connection=cassandra_connection)
+
+    # Update chat room message status.
+    update_chat_room_message_status(
+        cassandra_connection=cassandra_connection,
+        cql_arguments={
+            "column_name": message_status,
+            "chat_room_id": chat_room_id,
+            "message_id": message_id
+        }
+    )
+
+    # Define the variable that stores information about the last chat room message data.
+    chat_room_message_data = get_chat_room_message_data(
+        cassandra_connection=cassandra_connection,
+        cql_arguments={
+            "chat_room_id": chat_room_id,
+            "message_id": message_id
+        }
+    )
+
+    # Define variables that stores formatted information about the chat room message.
+    chat_room_message = analyze_and_format_chat_room_message_data(chat_room_message_data=chat_room_message_data)
+
+    # Return data of the chat room message.
     return chat_room_message
